0bbb1a868750842c2548c88029163ba82643f627
Mo <moziauddin@gmail.com>
2021-02-13 20:54:44 +1100

    Saved the file in user_home logs dir and change the log rotation policy

--- src/main/groovy/au/com/moziauddin/HomeController.groovy
+++ src/main/groovy/au/com/moziauddin/HomeController.groovy
@@ -1,5 +1,6 @@
 package au.com.moziauddin
 
+import groovy.util.logging.Slf4j
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.security.annotation.Secured
@@ -8,6 +9,7 @@ import io.micronaut.security.rules.SecurityRule
 import javax.annotation.security.PermitAll
 import javax.inject.Inject
 
+@Slf4j
 @Secured(SecurityRule.IS_AUTHENTICATED)
 @Controller("/home")
 class HomeController {
@@ -19,11 +21,13 @@ class HomeController {
     @PermitAll
     @Get('/')
     String index() {
+        log.info "Saying hello..."
         return homeService.sayHello()
     }
 
     @Get('/secret')
     String secretResponse() {
+        log.info "Secret was accessed"
         return 'MySecretPassword'
     }
 }
